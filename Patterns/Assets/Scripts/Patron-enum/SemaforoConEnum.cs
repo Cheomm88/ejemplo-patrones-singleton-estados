8038f4d8339b05939d0ff078f5879e1a2ecbd053
Cheomm88 <chopids@gmail.com>
2025-10-30 09:37:30 +0000

    Semáforo con enums comentado

--- Patterns/Assets/Scripts/Patron-enum/SemaforoConEnum.cs
+++ Patterns/Assets/Scripts/Patron-enum/SemaforoConEnum.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class SemaforoConEnum : MonoBehaviour
 {
+    //Declarando los diferentes estados de mi semáforo.
     public enum EstadoEnumSemaforo
     {
         enRojo,
@@ -10,52 +11,90 @@ public class SemaforoConEnum : MonoBehaviour
         enAmarillo
     }
 
-    public EstadoEnumSemaforo estadoInicial;
+    //Para poder seleccionar desde el inspector el estado inicial.
+    [SerializeField]
+    EstadoEnumSemaforo estadoInicial;
+    //Estado actual del semaforo NADIE lo puede cambiar por eso es privado.
     private EstadoEnumSemaforo estadoActualSemaforo;
 
-    public float timer = 0.0f;
+    //Contador de tiempo
+    float timer = 0.0f;
 
+    //Acceso a las luces.
     public GameObject luzRoja, luzVerde, luzAmarilla;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Pone el estado actual del semaforo en el que tengamos seleccionado en el inspector de unity.
     void Start()
     {
         estadoActualSemaforo = estadoInicial;
     }
 
-    // Update is called once per frame
+    //Cuenta tiempo y ejecuta el estado actual.
     void Update()
     {
         timer += Time.deltaTime;
 
+        //Un Switch es como un IF simplificado en este ejemplo lo que hace es 
+        //mirar el valor de estadoActualSemaforo y según el estado (rojo, verde, amarillo)
+        //Ejecuta la función de dicho estado.
         switch (estadoActualSemaforo)
         {
             case EstadoEnumSemaforo.enRojo:
                 SemaforoEnRojo();
                 break;
-
-            case EstadoEnumSemaforo.enVerde:
-                Debug.Log("En verde");
+            case EstadoEnumSemaforo.enAmarillo:
+                SemaforoEnAmarillo();
                 break;
-
+            case EstadoEnumSemaforo.enVerde:
+                SemaforoEnVerde();
+                break;
         }
-
     }
 
+    //Funciones de cada estado.
     void SemaforoEnRojo()
     {
-        if (timer < 10f)
-        {
-            luzRoja.SetActive(true);
-            luzVerde.SetActive(false);
-            luzAmarilla.SetActive(false);
-        }
-        else 
+        
+        luzRoja.SetActive(true);
+        luzVerde.SetActive(false);
+        luzAmarilla.SetActive(false);
+        
+        if(timer > 10.0f) 
         {
             estadoActualSemaforo = EstadoEnumSemaforo.enVerde;
             timer = 0.0f;
         }
     }
-    
+
+    void SemaforoEnVerde()
+    {
+        if (timer < 15f)
+        {
+            luzRoja.SetActive(false);
+            luzVerde.SetActive(true);
+            luzAmarilla.SetActive(false);
+        }
+        else
+        {
+            estadoActualSemaforo = EstadoEnumSemaforo.enAmarillo;
+            timer = 0.0f;
+        }
+    }
+
+    void SemaforoEnAmarillo()
+    {
+        if (timer < 2f)
+        {
+            luzRoja.SetActive(false);
+            luzVerde.SetActive(false);
+            luzAmarilla.SetActive(true);
+        }
+        else
+        {
+            estadoActualSemaforo = EstadoEnumSemaforo.enRojo;
+            timer = 0.0f;
+        }
+    }
+
 }
 

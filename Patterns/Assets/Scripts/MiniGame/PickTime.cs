c3e327adf1cb2659c0d21a71688d4c1a7d20f17b
Cheomm88 <chopids@gmail.com>
2025-10-30 12:11:31 +0000

    Added singleton example to minigame

--- Patterns/Assets/Scripts/MiniGame/PickTime.cs
+++ Patterns/Assets/Scripts/MiniGame/PickTime.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class PickTime : MonoBehaviour, IPickeableObject
 {
+    [SerializeField]
+    AudioClip clip;
     public void RecogerObjeto()
     {
         Debug.Log("Recoger tiempo");
+        AudioEffects.Instance.PlaySound(clip);
     }
 }

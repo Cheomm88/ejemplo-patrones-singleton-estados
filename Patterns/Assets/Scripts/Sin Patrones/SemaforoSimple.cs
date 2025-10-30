74233d6e2a7395190b91ba41a309f9f2ba4808a7
Cheomm88 <chopids@gmail.com>
2025-10-30 08:40:59 +0000

    Actualización código

--- Patterns/Assets/Scripts/Sin Patrones/SemaforoSimple.cs
+++ Patterns/Assets/Scripts/Sin Patrones/SemaforoSimple.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SemaforoSimple : MonoBehaviour
 {
     public GameObject luzRoja, luzAmarilla, luzVerde;
-
+    public float timer = 0f;
     void Start()
     {
         PonerEnRojo();
@@ -12,7 +12,15 @@ public class SemaforoSimple : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer > 10f && luzRoja.activeSelf == true)
+        {
+            PonerEnVerde();
+        }
+        else if (timer >= 15f && luzVerde.activeSelf == true)
+        {
+            PonerEnAmbar();
+        }
     }
 
     void PonerEnRojo()
@@ -21,6 +29,27 @@ public class SemaforoSimple : MonoBehaviour
         luzRoja.SetActive(true);
         luzAmarilla.SetActive(false);
         luzVerde.SetActive(false);
+        timer = 0f;
+
+    }
+
+    void PonerEnVerde()
+    {
+        //Estado inicial en rojo
+        luzRoja.SetActive(false);
+        luzAmarilla.SetActive(false);
+        luzVerde.SetActive(true);
+        timer = 0f;
+
+    }
+
+    void PonerEnAmbar()
+    {
+        //Estado inicial en rojo
+        luzRoja.SetActive(false);
+        luzAmarilla.SetActive(true);
+        luzVerde.SetActive(false);
+        timer = 0f;
 
     }
 }
